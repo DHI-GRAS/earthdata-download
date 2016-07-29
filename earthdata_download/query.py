a9df58e14bae6bd2e0c23b8cddde912faaa21d12
Jonas <josl@dhi-gras.com>
2016-07-29 10:56:26 +0200

    raise download limit to 2000 files

--- earthdata_download/query.py
+++ earthdata_download/query.py
@@ -1,11 +1,14 @@
 import json
 import requests
 
-nasa_echo_url_base = 'https://api.echo.nasa.gov/catalog-rest/echo_catalog/granules.json?page_num=1&page_size=100'
+nasa_echo_url_base = 'https://api.echo.nasa.gov/catalog-rest/echo_catalog/granules.json?page_num=1'
 
 def url_from_query(short_name='', version='', date_range=(), extent={}):
 
     url = nasa_echo_url_base
+
+    # currently the API only allows for 2000 files at a time
+    url += '&page_size=2000'
 
     if short_name:
         url += '&short_name={}'.format(short_name)
