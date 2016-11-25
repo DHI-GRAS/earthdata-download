313b5a2cd8dbf2735d023417598487a378f31dd5
Jonas <josl@dhi-gras.com>
2016-11-25 15:11:02 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='earthdata_download',
+      version='0.1',
+      description='NASA EarthData download interface',
+      author='Jonas Solvsteen',
+      author_email='josl@dhi-gras.com',
+      url='https://www.dhi-gras.com',
+      packages=find_packages(),
+      )
