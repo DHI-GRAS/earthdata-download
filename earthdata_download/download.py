e3367f895763163b71806674a70026159169bf3a
Jonas <josl@dhi-gras.com>
2017-01-12 16:12:03 +0100

    user wget_provider

--- earthdata_download/download.py
+++ earthdata_download/download.py
@@ -1,64 +1,22 @@
 import os
 import posixpath
-import subprocess
-import re
 import logging
 
-wget_exe = os.path.join(os.path.dirname(__file__), 'wget64.exe')
+from wget_provider import download_url
 
-def download_data(url, username, password, download_dir='.', local_filename='',
-        logger=None):
+logger = logging.getLogger('earthdata_download.download')
+
+
+def download_data(url, username, password, download_dir='.', local_filename=''):
     """Download a url with login using wget"""
-
-    if logger is None:
-        logger = logging.getLogger('earthdata_download')
-        logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
 
     # generate local file name
     if not local_filename:
         local_filename = os.path.join(download_dir, posixpath.basename(url))
 
-    # put together command
-    cmd = [wget_exe, '-L', '--continue']
-    cmd += ['--user='+username, '--password='+password]
-    cmd += ['-O', local_filename]
-    cmd += [url]
-
-    logger.debug('Download command is \'{}\'.'.format(' '.join(cmd)))
-
-    # on windows, suppress wget window
-    startupinfo = None
-    if os.name == 'nt':
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-    # execute command
-    proc = subprocess.Popen(cmd,
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE,
-            startupinfo=startupinfo)
-    n = 0
-    last_percent = ''
-    for line in iter(proc.stdout.readline, ''):
-        n += 1
-        if n <= 10:
-            # print the first 10 lines
-            logger.info(line.rstrip())
-        elif '%' in line:
-            # get and log status
-            try:
-                percent = re.search('(\d{1,3}\%)', line).group(0)
-                if percent != last_percent:
-                    logger.info(percent)
-                    last_percent = percent
-            except IndexError:
-                pass
-        if 'ERROR' in line:
-            # fail on errors
-            raise RuntimeError('Download of {} failed: {}'.format(url, line.rstrip()))
+    download_url(url,
+            auth=dict(username=username, password=password),
+            continue_partial=True,
+            local_filename=local_filename)
 
     return local_filename
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='earthdata_download',
-      version='0.1',
-      description='NASA EarthData download interface',
-      author='Jonas Solvsteen',
-      author_email='josl@dhi-gras.com',
-      url='https://www.dhi-gras.com',
-      packages=find_packages(),
-      )
+setup(
+    name='earthdata_download',
+    version='0.1',
+    description='NASA EarthData download interface',
+    author='Jonas Solvsteen',
+    author_email='josl@dhi-gras.com',
+    url='https://www.dhi-gras.com',
+    packages=find_packages(),
+    install_requires=['wget_provider'],
+    dependency_links=['https://github.com/DHI-GRAS/wget_provider.git'])
