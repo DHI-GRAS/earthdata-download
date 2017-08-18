74f8538a5569060c23cd30f023c0ebf701590528
Jonas <josl@dhi-gras.com>
2017-08-18 09:26:10 +0200

    change date logic

--- earthdata_download/query.py
+++ earthdata_download/query.py
@@ -7,8 +7,8 @@ import requests
 logger = logging.getLogger('earthdata_download.query')
 
 nasa_echo_url_base = 'https://api.echo.nasa.gov/catalog-rest/echo_catalog/granules.json?page_num=1'
-
 max_n_products = 2000
+
 
 def url_from_query(short_name='', version='', date_range=(), extent={}, n_products=max_n_products):
     """Generate EarthData query url for given parameters
@@ -34,7 +34,9 @@ def url_from_query(short_name='', version='', date_range=(), extent={}, n_produc
     url = nasa_echo_url_base
 
     if n_products > max_n_products:
-        raise ValueError('Currently, the API only allows for {} products at a time'.format(max_n_products))
+        raise ValueError(
+                'Currently, the API only allows for {} products at a time'
+                ''.format(max_n_products))
     url += '&page_size={}'.format(n_products)
 
     if short_name:
@@ -44,7 +46,9 @@ def url_from_query(short_name='', version='', date_range=(), extent={}, n_produc
         url += '&version={}'.format(version)
 
     if date_range:
-        url += '&temporal[]={},{}'.format(*[d.strftime('%Y-%m-%dT23:59:59') for d in date_range])
+        datefmt = '%Y-%m-%dT%H:%M:%S'
+        start_date, end_date = _get_dates(date_range)
+        url += '&temporal[]={},{}'.format(*[d.strftime(datefmt) for d in (start_date, end_date)])
 
     if extent:
         url += '&bounding_box={xmin},{ymin},{xmax},{ymax}'.format(**extent)
@@ -68,16 +72,26 @@ def get_entries_from_url(url):
     except KeyError:
         raise ValueError('Query with url \'{}\' did not return any files. '
                 'Catalogue:\n\n{}'.format(url, catalogue.__repr__()))
-
     return entries
 
 
-def get_data_urls_from_entries(entries):
-    all_href = [e['links'][0]['href'] for e in entries]
+def get_data_urls_from_entries(entries, linkno=0):
+    """Get data URLS from entries
+
+    Parameters
+    ----------
+    entries : list of dict
+        entries from JSON response
+    linkno : int
+        number of link to get
+        0: HDF over ftp
+        1: HDF over https
+    """
+    all_href = [e['links'][linkno]['href'] for e in entries]
     return [url for url in all_href if not url.endswith('.jpg')]
 
 
-def get_data_urls(url):
+def get_data_urls(url, linkno=0):
     """Get data urls from query url
 
     Parameters
@@ -86,7 +100,7 @@ def get_data_urls(url):
         REST query url
     """
     entries = get_entries_from_url(url)
-    return get_data_urls_from_entries(entries)
+    return get_data_urls_from_entries(entries, linkno=linkno)
 
 
 def date_from_timestamp(s):
@@ -104,7 +118,7 @@ def get_entry_end_date(e):
     return date_from_timestamp(e['time_end'])
 
 
-def find_data(short_name='', version='', start_date=None, end_date=None, extent={}):
+def find_data(short_name='', version='', start_date=None, end_date=None, extent={}, linkno=0):
     """Query EarthData for products and return download urls
 
     Parameters
@@ -115,9 +129,14 @@ def find_data(short_name='', version='', start_date=None, end_date=None, extent=
         product version (e.g. '005', leading zeros matter!)
     date_range : (datetime.datetime, datetime.datetime)
         date range
+        at least one must be specified
     extent : dict
         extent dictionary
         must have entries xmin, xmax, ymin, ymax
+    linkno : int
+        number of link to get
+        0: HDF over ftp
+        1: HDF over https
     """
     if start_date is None:
         # retrieve first avaliable product date
@@ -146,7 +165,7 @@ def find_data(short_name='', version='', start_date=None, end_date=None, extent=
         except ValueError:
             # no entries found
             break
-        data_urls += get_data_urls_from_entries(entries)
+        data_urls += get_data_urls_from_entries(entries, linkno=linkno)
 
         # check if there is more
         if len(entries) < max_n_products:
@@ -165,3 +184,19 @@ def find_data(short_name='', version='', start_date=None, end_date=None, extent=
         continue
 
     return data_urls
+
+
+def _get_dates(date_range):
+    """Derive start_date and end_date from date_range"""
+    start_date, end_date = date_range
+    if start_date is None and end_date is None:
+        raise ValueError('Either start_date or end_date must be provided.')
+    if end_date is None:
+        end_date = start_date + datetime.timedelta(days=1)
+    elif start_date is None:
+        start_date = end_date - datetime.timedelta(days=1)
+    t0 = datetime.time()
+    if start_date.time() == t0:
+        start_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0)
+        end_date = end_date.replace(hour=23, minute=59, second=59)
+    return start_date, end_date
