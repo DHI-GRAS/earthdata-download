7960b8d42239f98ddb2057ccd8edd4b354428617
Jonas <josl@dhi-gras.com>
2017-09-12 21:18:19 +0200

    remove wget dependency and bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='earthdata_download',
-    version='0.4',
+    version='0.5',
     description='NASA EarthData download interface',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
     url='https://www.dhi-gras.com',
     packages=find_packages(),
-    install_requires=[
-        'wget_provider'],
-    dependency_links=[
-        'https://github.com/DHI-GRAS/wget_provider/archive/v0.3.tar.gz#egg=wget_provider-0.3'])
+    install_requires=['requests'])
