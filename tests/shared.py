56d30f02f31f28ba7c404b66f3535bd6fc1e1c7a
Jonas Sølvsteen <j08lue@gmail.com>
2018-01-03 10:29:31 +0100

    hm

--- tests/shared.py
+++ tests/shared.py
@@ -1,16 +1,15 @@
+import os
 import types
 import warnings
-from os import environ
-from os.path import abspath, dirname
 
 import pytest
 import vcr
 
 from .custom_serializer import BinaryContentSerializer
 
-TESTS_DIR = dirname(abspath(__file__))
-FIXTURES_DIR = dirname(abspath(__file__)) + '/fixtures'
-CASSETTE_DIR = FIXTURES_DIR + '/vcr_cassettes/'
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_DIR = os.path.join(TESTS_DIR, 'fixtures')
+CASSETTE_DIR = os.path.join(FIXTURES_DIR, 'vcr_cassettes')
 
 vcr_option = pytest.config.getoption("--vcr")
 record_mode = "none"
@@ -52,8 +51,7 @@ if vcr_option != "disable":
         filter_headers=['Set-Cookie'],
         before_record_request=scrub_request,
         before_record_response=scrub_response,
-        decode_compressed_response=True
-    )
+        decode_compressed_response=True)
     my_vcr.register_serializer('custom', BinaryContentSerializer(CASSETTE_DIR))
     my_vcr.serializer = 'custom'
     my_vcr.register_matcher('range_header', range_header_matcher)
@@ -81,7 +79,7 @@ else:
     my_vcr = DummyVCR()
 
 if (vcr_option != "use" and
-        ('EARTHDATA_USER' not in environ or 'EARTHDATA_PASSWORD' not in environ)):
+        ('EARTHDATA_USER' not in os.environ or 'EARTHDATA_PASSWORD' not in os.environ)):
     warnings.warn(
         "Credentials are not set while not using prerecorded queries: "
         "please set EARTHDATA_USER and EARTHDATA_PASSWORD environment variables.")
