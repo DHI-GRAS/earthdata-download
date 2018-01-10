e04e651f6e64bcab7e2ff2bed2ba3d8819243a6e
Jonas Sølvsteen <j08lue@gmail.com>
2018-01-10 20:41:59 +0100

    assert that download folder is right

--- tests/test_api_download.py
+++ tests/test_api_download.py
@@ -14,8 +14,9 @@ def test_api_init_with_credentials(earthdata_credentials):
 def test_download_single(api_query_kw, earthdata_credentials, tmpdir):
     api = EarthdataAPI(**earthdata_credentials)
     data_urls = api.query(**api_query_kw)
-    tempdir = str()
+    tempdir = str(tmpdir)
 
     local_filename = api.download_single(data_urls[0], download_dir=tempdir)
     assert os.path.isfile(local_filename)
+    assert os.path.abspath(os.path.dirname(local_filename)) == os.path.abspath(tempdir)
     assert os.path.getsize(local_filename) > 1e6
