dc85f9f0cd7826d4401135d63ec29a2f8469beae
Jonas <j08lue@gmail.com>
2018-03-16 11:10:57 +0100

    make data url finding more configurable (#17)

--- tests/test_download.py
+++ tests/test_download.py
@@ -9,14 +9,13 @@ from .shared import my_vcr
 def test_download(query_kw, earthdata_credentials, tmpdir):
     from earthdata_download import query
     from earthdata_download import download
-    tempdir = str(tmpdir)
     entries = query.get_entries(**query_kw)
     local_filename = download.download_entry(
         entries[0],
-        download_dir=tempdir,
+        download_dir=str(tmpdir),
         **earthdata_credentials)
     assert os.path.isfile(local_filename)
-    assert os.path.abspath(os.path.dirname(local_filename)) == os.path.abspath(tempdir)
+    assert os.path.abspath(os.path.dirname(local_filename)) == os.path.abspath(str(tmpdir))
     assert os.path.getsize(local_filename) > 1e6
 
 
@@ -29,3 +28,15 @@ def test_download_gesdisc_unauthorized(query_kw, earthdata_credentials, tmpdir):
         'TRMM_RT/TRMM_3B42RT_Daily.7/2016/12/3B42RT_Daily.20161231.7.nc4')
     with pytest.raises(RuntimeError):
         download.download_url(gesdisc_url, download_dir=str(tmpdir), **earthdata_credentials)
+
+
+@my_vcr.use_cassette
+@pytest.mark.nasa
+def test_find_data_url(query_kw):
+    from earthdata_download import query
+    from earthdata_download import download
+    entries = query.get_entries(**query_kw)
+    e = entries[0]
+    print(e)
+    download.data_url_from_entry(e)
+    download.data_url_from_entry(e, url_match='*.hdf')
